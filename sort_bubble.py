aabaf1c4e3f4c30a5cce7205066eb7b374073965
Anas Zughayyar <anaszughayyar@neobook.local>
2024-10-22 13:01:58 +0300

    new

---
+++ sort_bubble.py
@@ -0,0 +1,21 @@
+########################
+#### Anas Zughayyar ####
+########################
+##### Bubble Sort #####
+########################
+
+def sort_bubble(some_list):
+    n = len(some_list)
+    for i in range(0,n):
+        for j in range(0,n-i-1):
+            if some_list[j] > some_list[j+1]:
+                temp = some_list[j]
+                some_list[j] = some_list[j+1]
+                some_list[j+1] = temp
+    return some_list
+
+list_1 = [8,5,2,6,9,3,1,4,0,7]
+list_2 = [81,53,12,-6,53,3,1,4,-2,7]
+
+print(sort_bubble(list_1))
+print(sort_bubble(list_2))
